344d48a606a5b7ca07c1e2a21a2463335e43c0eb
leon-taquet <62070746+leon-taquet@users.noreply.github.com>
2020-11-30 14:02:39 -0600

    newTrip.php
    Adds new trip to mySQL from a posted form containing name, default currency, and userID.
    
    Form fields should be name, def_curr, and a hidden field containing userID and named userID.

--- newTrip.php
+++ newTrip.php
@@ -2,13 +2,34 @@
 $servername = "localhost";
 $username = "id14882043_ltaque01";
 $password = "WilliamLeonKateriJulia4!";
+$database = "id14882043_itet";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+// echo "Connected successfully";
+
+// Add trip
+
+extract ($_POST);
+// $userID;
+// $name;
+// $def_curr;
+
+// $userID = 0;
+// $name = "test";
+// $def_curr = "USD";
+
+$sql = "INSERT INTO trips (tripname, default_currency, userID) VALUES ('$name', '$def_curr', $userID)";
+
+// if ($conn->query($sql) === TRUE) {
+//   echo "New record created successfully";
+// } else {
+//   echo "Error: " . $sql . "<br>" . $conn->error;
+// }
+
 ?>
