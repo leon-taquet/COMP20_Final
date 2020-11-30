1ae1304d1d60ea5fbec41fc47a581aa2e323ff74
leon-taquet <62070746+leon-taquet@users.noreply.github.com>
2020-11-30 14:03:19 -0600

    Update mySQL_connect_template.php

--- mySQL_connect_template.php
+++ mySQL_connect_template.php
@@ -2,9 +2,10 @@
 $servername = "localhost";
 $username = "id14882043_ltaque01";
 $password = "WilliamLeonKateriJulia4!";
+$database = "id14882043_itet";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
