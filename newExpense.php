d8aac596bd569a3fd441fbd813a6675e70d1313c
Kateri L <kateri.lalicker@tufts.edu>
2020-12-06 13:25:02 -0500

    Adding expenses works!

--- newExpense.php
+++ newExpense.php
@@ -1,36 +1,54 @@
-<?php
-$servername = "localhost";
-$username = "id14882043_ltaque01";
-$password = "WilliamLeonKateriJulia4!";
-$database = "id14882043_itet";
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+        $servername = "localhost";
+        $username = "id14882043_ltaque01";
+        $password = "WilliamLeonKateriJulia4!";
+        $database = "id14882043_itet";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $database);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-// echo "Connected successfully";
+        // Check connection
+        if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+        }
+        // echo "Connected successfully";
 
-// Add trip
+        // Add trip
 
-extract ($_POST);
-// $userID;
-// $name;
-// $def_curr;
+        extract ($_POST);
+        // $expenseN;
+        // $expenseT;
+        // $expenseForeign; (Stored as a float)
+        // $expenseHome; (Stored as a float)
+        // $date;
+        $expenseF = 40.00;
+        $expenseH = 10.00;
+        $trip = 1;
+        $local_currency = "USD";
+        $category = 1;
 
-// $userID = 0;
-// $name = "test";
-// $def_curr = "USD";
+        $sql = "INSERT INTO expenses (expense_name, cost_home, cost_local,
+            local_currency, CategoryID, tripID) VALUES ('$expenseN',
+                '$expenseH', '$expenseF', '$local_currency',
+                '$category', '$trip')";
+        $conn->query($sql);
 
-$sql = "INSERT INTO trips (tripname, default_currency, userID) VALUES ('$tripName', '$defaultCurrency', '$userID')";
-$conn->query($sql);
+        $sql = "SELECT expense_date FROM expenses WHERE tripID=1";
+        $result = $conn->query($sql);
+        $date = $result->fetch_row()[0];
+        echo $date . " This is the date.";
+        // if ($conn->query($sql) === TRUE) {
+        //   echo "New record created successfully";
+        // } else {
+        //   echo "Error: " . $sql . "<br>" . $conn->error;
+        // }
 
-// if ($conn->query($sql) === TRUE) {
-//   echo "New record created successfully";
-// } else {
-//   echo "Error: " . $sql . "<br>" . $conn->error;
-// }
-
-?>
+        ?>
+    </body>
+</html>
