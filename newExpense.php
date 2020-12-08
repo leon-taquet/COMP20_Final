b7a09c44fb4795ecd98bca88160b5ece81db9fc5
Kateri L <kateri.lalicker@tufts.edu>
2020-12-08 14:15:41 -0500

    Update newExpense.php

--- newExpense.php
+++ newExpense.php
@@ -1,53 +1,53 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <?php
-        $servername = "localhost";
-        $username = "id14882043_ltaque01";
-        $password = "WilliamLeonKateriJulia4!";
-        $database = "id14882043_itet";
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <?php
+    $servername = "localhost";
+    $username = "id14882043_ltaque01";
+    $password = "WilliamLeonKateriJulia4!";
+    $database = "id14882043_itet";
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $database);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
 
-        // Check connection
-        if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-        }
-        // echo "Connected successfully";
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    // echo "Connected successfully";
 
-        // Add trip
+    // Add trip
 
-        extract ($_POST);
-        // $expenseN;
-        // $expenseT; (NOT USED currently)
-        // $expenseForeign; (Stored as a float)
-        // $expenseHome; (Stored as a float)
-        // $date;
-        $trip = 1;
-        $local_currency = "GBP";
-        $category = 1;
+    extract ($_POST);
+    // $expenseN;
+    // $expenseT; (NOT USED currently)
+    // $expenseForeign; (Stored as a float)
+    // $expenseHome; (Stored as a float)
+    // $date;
+    $trip = 1;
+    $local_currency = "GBP";
+    $category = 1;
 
-        $sql = "INSERT INTO expenses (expense_name, cost_home, cost_local,
-            local_currency, CategoryID, tripID) VALUES ('$expenseN',
-                '$expenseHome', '$expenseForeign', '$local_currency',
-                '$category', '$trip')";
-        $conn->query($sql);
+    $sql = "INSERT INTO expenses (expense_name, cost_home, cost_local,
+        local_currency, CategoryID, tripID) VALUES ('$expenseN',
+            '$expenseHome', '$expenseForeign', '$local_currency',
+            '$category', '$trip')";
+    $conn->query($sql);
 
-        // $sql = "SELECT expense_date FROM expenses WHERE tripID=1";
-        // $result = $conn->query($sql);
-        // $date = $result->fetch_row()[0];
-        echo $date . " This is the date.";
-        // if ($conn->query($sql) === TRUE) {
-        //   echo "New record created successfully";
-        // } else {
-        //   echo "Error: " . $sql . "<br>" . $conn->error;
-        // }
+    // $sql = "SELECT expense_date FROM expenses WHERE tripID=1";
+    // $result = $conn->query($sql);
+    // $date = $result->fetch_row()[0];
+    echo $date . " This is the date.";
+    // if ($conn->query($sql) === TRUE) {
+    //   echo "New record created successfully";
+    // } else {
+    //   echo "Error: " . $sql . "<br>" . $conn->error;
+    // }
 
-        ?>
-        <p></p>
-    </body>
+    ?>
+    <p>Expense successfully inserted!</p>
+</body>
 </html>
