2382f05700d94e0624ec23b66baaf1a69b6eaedc
leon-taquet <62070746+leon-taquet@users.noreply.github.com>
2020-12-12 18:24:16 -0500

    delete-expense php file

---
+++ delete-expense.php
@@ -0,0 +1,61 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+     <script>
+            window.onload = function(){
+                document.forms[0].submit();
+            }
+    </script>
+    <meta charset="utf-8">
+    <?php
+    $servername = "localhost";
+    $usernameData = "id14882043_ltaque01";
+    $passwordData = "WilliamLeonKateriJulia4!";
+    $database = "id14882043_itet";
+    // Create connection
+    $conn = new mysqli($servername, $usernameData, $passwordData, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    //echo "Connected successfully";
+
+    // Add trip
+    // foreach($_POST as $key=>$value){
+    // echo $key.' '.$value."\n";
+    // }
+
+    // Have username and password
+
+    // prepare and bind
+    //extract($_POST)
+    $trip = $_SESSION["tripID"];
+    $tripname = $_SESSION["tripname"];
+
+
+    $sql = "DELETE FROM expenses WHERE ID = $expenseID";
+
+    $conn->query($sql);
+
+
+      $msg = "<h1> Expense deleted... Adding to Trip Page </h1>";
+      $form = "<form method = 'post' action = 'trip.php'> <input type = 'hidden' name = 'tripid' value = $trip >
+      <input type = 'hidden' name = 'tripname' value = '$tripname' >
+      </form>";
+
+
+    $conn->close();
+    ?>
+    <title></title>
+  </head>
+  <body>
+    <?php
+        //echo $msg;
+        echo $form;
+     ?>
+
+
+  </body>
+</html>
