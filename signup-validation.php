d0a4b125ed59c18779bf7c98de74414bbeb4eeb3
Kateri L <kateri.lalicker@tufts.edu>
2020-12-13 13:00:53 -0500

    Code Formatting

--- signup-validation.php
+++ signup-validation.php
@@ -1,56 +1,56 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <?php
-    $servername = "localhost";
-    $usernameData = "id14882043_ltaque01";
-    $passwordData = "WilliamLeonKateriJulia4!";
-    $database = "id14882043_itet";
-    // Create connection
-    $conn = new mysqli($servername, $usernameData, $passwordData, $database);
+<head>
+<meta charset="utf-8">
+<?php
+$servername = "localhost";
+$usernameData = "id14882043_ltaque01";
+$passwordData = "WilliamLeonKateriJulia4!";
+$database = "id14882043_itet";
+// Create connection
+$conn = new mysqli($servername, $usernameData, $passwordData, $database);
 
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
-    // echo "Connected successfully";
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-    // Add trip
+// Add trip
+extract ($_POST);
 
-    extract ($_POST);
-    // Have username password
+// Have username password
+$sql = "SELECT ID FROM users WHERE username = '$username'";
+$result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
+    // output data of each row
+    $msg = "<h1> Username is taken... Sending back to Sign Up Page </h1>";
+    echo "<meta http-equiv='refresh'" .
+        "content='2;URL=http://aboutlct.000webhostapp.com/Final/signup.php'/>";
+}
+else {
+    $sql = "INSERT INTO users (username, HomeCurrency, password, name)" .
+        "VALUES ('$username', '$hcurrency', '$password' ,'$name')";
+    $conn->query($sql);
     $sql = "SELECT ID FROM users WHERE username = '$username'";
     $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
-      // output data of each row
-      $msg = "<h1> Username is taken... Sending back to Sign Up Page </h1>";
-      echo "<meta http-equiv='refresh' content='2;URL=http://aboutlct.000webhostapp.com/Final/signup.php' />";
+    $msg = "<h1> Successful Created Account... Forwarding to Trip Page </h1>";
+    echo "<meta http-equiv='refresh' content='2;" .
+        "URL=http://aboutlct.000webhostapp.com/Final/dashboard.php' />";
+    $userID = $result->fetch_row()[0];
+    $_SESSION["HomeCurrency"] = $hcurrency;
+    $_SESSION["userID"] = $userID;
+}
+$conn->close();
+?>
+<title>Loading...</title>
+</head>
 
-    } else {
-      $sql = "INSERT INTO users (username, HomeCurrency, password, name) VALUES ('$username', '$hcurrency', '$password' ,'$name')";
-      $conn->query($sql);
-      $sql = "SELECT ID FROM users WHERE username = '$username'";
-      $result = $conn->query($sql);
-
-      $msg = "<h1> Successful Created Account... Forwarding to Trip Page </h1>";
-      echo "<meta http-equiv='refresh' content='2;URL=http://aboutlct.000webhostapp.com/Final/dashboard.php' />";
-      $userID = $result->fetch_row()[0];
-      $_SESSION["HomeCurrency"] = $hcurrency;
-      $_SESSION["userID"] = $userID;
-    }
-    $conn->close();
-    ?>
-    <title></title>
-  </head>
-  <body>
-    <?php
-        echo $msg;
-     ?>
-
-
-  </body>
+<body>
+<?php
+    echo $msg;
+?>
+</body>
 </html>
--- signup.php
+++ signup.php
@@ -1,75 +1,78 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="Stylesheet.css">
-    <style>
-        input{
-            font-family: Times New Roman;
-            font-variant: none;
-        }
-    </style>
-    <?php
-        $servername = "localhost";
-        $usernameData = "id14882043_ltaque01";
-        $passwordData = "WilliamLeonKateriJulia4!";
-        $database = "id14882043_itet";
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<title>New User Sign Up</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="Stylesheet.css">
+<style>
+    input {
+        font-family: Times New Roman;
+        font-variant: none;
+    }
+</style>
+<?php
+$servername = "localhost";
+$usernameData = "id14882043_ltaque01";
+$passwordData = "WilliamLeonKateriJulia4!";
+$database = "id14882043_itet";
 
-        // Create connection
-        $conn = new mysqli($servername, $usernameData, $passwordData, $database);
+// Create connection
+$conn = new mysqli($servername, $usernameData, $passwordData, $database);
 
-        // Check connection
-        if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-        }
-        $sql = "SELECT DISTINCT codes FROM codes ORDER BY codes";
-        $result = $conn->query($sql);
-        ?>
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT DISTINCT codes FROM codes ORDER BY codes";
+$result = $conn->query($sql);
+?>
 </head>
 <body>
-    <header><a class="header" href="login.html">International Travel Expense Tracker</a></header>
+<header><a class="header" href="login.html">International Travel Expense
+    Tracker</a></header>
 
-    <nav>
-        <ul>
-            <div class="leftnav">
-                <li><a href = "about.html">About</a></li>
-                <div class="rightnav">
-                    <li><a href = "signup.html" class= "currpage"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                    <li><a href = "login.html"><span class="glyphicon glyphicon-log-in"></span> Login</a> </li>
-                </div>
+<nav>
+    <ul>
+        <div class="leftnav">
+            <li><a href = "about.html">About</a></li>
+            <div class="rightnav">
+                <li><a href = "signup.html" class= "currpage">
+                    <span class="glyphicon glyphicon-user"></span> Sign Up</a>
+                </li>
+                <li><a href = "login.html">
+                    <span class="glyphicon glyphicon-log-in"></span> Login</a>
+                </li>
             </div>
-        </ul>
-    </nav>
-    <div class="logcontainer">
-        <div class="login">
-            <h1>Sign Up</h1>
-            <h3>Please fill in all fields below:<br></h3>
-            <form method="post" action="signup-validation.php" >
-                <br>
-                Username*: <input type="text" name="username" value=""/><br><br>
-                Password*: <input type="Password" name="password" value=""/><br><br>
-                Name*: <input type="text" name="name" value=""/><br><br>
-                Home Currency: <select name = "hcurrency" size = '1'>
-                                <?php
-                                foreach($result as $row){
-                                    if ($row['codes'] == 'USD')
-                                      echo "<option value = '".$row['codes']."' selected>". $row['codes'] ." </option>";
-                                    else
-                                      echo "<option value = '".$row['codes']."'>". $row['codes'] ." </option>";
-                                }
-                                ?>
-                                </select><br><br>
-                <input type="submit" value="Sign Up"/>
-            </form>
-
         </div>
+    </ul>
+</nav>
+<div class="logcontainer">
+    <div class="login">
+        <h1>Sign Up</h1>
+        <h3>Please fill in all fields below:<br></h3>
+        <form method="post" action="signup-validation.php" >
+            <br>
+            Username*: <input type="text" name="username" value=""/><br><br>
+            Password*: <input type="Password" name="password" value=""/><br><br>
+            Name*: <input type="text" name="name" value=""/><br><br>
+            Home Currency: <select name = "hcurrency" size = '1'>
+            <?php
+            foreach($result as $row) {
+                if ($row['codes'] == 'USD')
+                  echo "<option value = '" . $row['codes'] . "' selected>" .
+                      $row['codes'] . " </option>";
+                else
+                  echo "<option value = '" . $row['codes'] . "'>" .
+                    $row['codes'] . " </option>";
+            }
+            ?>
+            </select><br><br>
+            <input type="submit" value="Sign Up"/>
+        </form>
     </div>
+</div>
 
-
-    <footer>ITET</footer>
-
-
+<footer>ITET</footer>
 </body>
